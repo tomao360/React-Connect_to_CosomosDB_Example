c25f99a2e94a19c190dcdc1c1ed5441651ace44d
tomao360 <tomao360@gmail.com>
2023-03-24 15:55:45 +0300

    React with CosmosDB example

--- example-cosmosdb/src/services/config.js
+++
@@ -1,3 +0,0 @@
-export const Endpoint = "https://localhost:8081";
-export const Key =
-  "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
--- example-cosmosdb/src/services/connectToCosomosDB.service.js
+++ example-cosmosdb/src/services/connectToCosomosDB.service.js
@@ -1,9 +1,8 @@
-import { Endpoint, Key } from "./config";
-
 const { CosmosClient } = require("@azure/cosmos");
 
-const endpoint = Endpoint;
-const key = Key;
+const endpoint = "https://localhost:8081";
+const key =
+  "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
 const databaseName = "northwindProducts";
 const containerName = "products";
 
